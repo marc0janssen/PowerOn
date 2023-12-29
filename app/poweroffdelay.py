9a30c5a14f50ec7d27452f51b7565175bd4d1316
Marco <marco@mjanssen.nl>
2023-12-30 00:54:52 +0100

    poweroffdelay

--- app/poweroffdelay.py
+++ app/poweroffdelay.py
@@ -383,8 +383,8 @@ class POD():
                         self.writeLog(
                             False, "PowerOff - Marking message for delete.\n")
 
-                        # if not self.dry_run:
-                            # imap.store(str(i), "+FLAGS", "\\Deleted")
+                        if not self.dry_run:
+                            imap.store(str(i), "+FLAGS", "\\Deleted")
 
                     else:
                         if self.verbose_logging:
