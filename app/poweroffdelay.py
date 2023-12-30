527c2f01d3fa72bd9dcb81daa368fbb1b2476ef9
Marco <marco@mjanssen.nl>
2023-12-30 18:57:28 +0100

    test

--- app/poweroffdelay.py
+++ app/poweroffdelay.py
@@ -304,12 +304,11 @@ class POD():
                                     f"{match.group(0)}\n"
                                 )
 
-                            rtext = ["marco@mjanssen.nl",
-                                     "janssenmaj@gmail.com",
-                                     "hij@daniellemarco.nl"]  # match.group(0)
+                            receiver_email = ["marco@mjanssen.nl", "janssenmaj@gmail.com", "hij@daniellemarco.nl"]  # match.group(0)
 
                             sender_email = self.mail_sender
-                            receiver_email = ", ".join(rtext)
+                            # receiver_email = ", ".join(rtext)
+
                             message = MIMEMultipart()
                             message["From"] = sender_email
                             message['To'] = receiver_email
