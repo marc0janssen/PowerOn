f69586434e119e77803f7dc2bf23656478bd4488
Marco <marco@mjanssen.nl>
2024-01-10 18:38:11 +0100

    test

--- app/poweroffbymail.py
+++ app/poweroffbymail.py
@@ -224,7 +224,7 @@ class POBE():
                                                     f"{self.nodeuser}"
                                                     f"@{self.nodeip}",
                                                     f"echo \"{ecsapedpwd}\""
-                                                    f"|sudo -S ls -al;exit"],
+                                                    f"|sudo -S touch /tmp/reboot.txt;exit"],
                                                 capture_output=True, text=True)
 
                                             # Print the command output
